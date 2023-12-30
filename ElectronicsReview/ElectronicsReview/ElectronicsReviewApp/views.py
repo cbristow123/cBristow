5b2e320045d5d48b7d1ed50732b1569dbf2e4604
cbristow123 <callumbristow11@gmail.com>
2023-12-30 18:26:36 +0000

    First basic view, url re-routing

--- ElectronicsReview/ElectronicsReview/ElectronicsReviewApp/views.py
+++ ElectronicsReview/ElectronicsReview/ElectronicsReviewApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def home (request):
+    return HttpResponse ('<H1> Electronic Reviews Home Page')
+
 # Create your views here.
